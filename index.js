1f4cb5cb302ce119b12f4e97cdb1b010cc41d52a
Samuel Lakra <samuellakra1290@gmail.com>
2024-10-25 20:43:15 +0530

    onclick function for each product

--- index.js
+++ index.js
@@ -43,3 +43,24 @@ function initGoogleSignIn() {
 
 // Initialize the Google Sign-In on page load
 window.onload = initGoogleSignIn;
+
+//sproduct function
+function sproductFunction(event){
+    window.location.href = "sproduct.html";
+}
+
+var MainImg = document.getElementById("MainImg");
+var smallimg = document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function(){
+    MainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function(){
+    MainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function(){
+    MainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function(){
+    MainImg.src = smallimg[3].src;
+}
