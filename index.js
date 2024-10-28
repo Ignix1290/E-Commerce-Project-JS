49268a6262ba31e9508ac55c415ed9641ed0c213
Samuel Lakra <samuellakra1290@gmail.com>
2024-10-28 20:24:59 +0530

    dynamic data retrival done

--- index.js
+++ index.js
@@ -14,6 +14,123 @@ if(close){
     })
 }
 
+//Shop Array
+const shop = [
+    {
+        brand: "adidas",
+        name: "Shirt Half-Sleeve",
+        image: "./images/products/f1.jpg",
+        rating: 5,
+        price: 78
+    },
+    {
+        brand: "adidas",
+        name: "Shirt Half-Sleeve",
+        image: "./images/products/f2.jpg",
+        rating: 5,
+        price: 58
+    },
+    {
+        brand: "adidas",
+        name: "Shirt Half-Sleeve",
+        image: "./images/products/f3.jpg",
+        rating: 5,
+        price: 68
+    },
+    {
+        brand: "adidas",
+        name: "Shirt Half-Sleeve",
+        image: "./images/products/f4.jpg",
+        rating: 5,
+        price: 60
+    },
+    {
+        brand: "adidas",
+        name: "Shirt Half-Sleeve",
+        image: "./images/products/f5.jpg",
+        rating: 5,
+        price: 65
+    },
+    {
+        brand: "adidas",
+        name: "Shirt Full-Sleeve",
+        image: "./images/products/f6.jpg",
+        rating: 5,
+        price: 88
+    },
+    {
+        brand: "adidas",
+        name: "Girls Pant",
+        image: "./images/products/f7.jpg",
+        rating: 5,
+        price: 50
+    },
+    {
+        brand: "adidas",
+        name: "Girls Top",
+        image: "./images/products/f8.jpg",
+        rating: 5,
+        price: 55
+    },
+    {
+        brand: "adidas",
+        name: "Shirt Full-Sleeve",
+        image: "./images/products/n1.jpg",
+        rating: 5,
+        price: 63
+    },
+    {
+        brand: "adidas",
+        name: "Shirt Full-Sleeve",
+        image: "./images/products/n2.jpg",
+        rating: 5,
+        price: 67
+    },
+    {
+        brand: "adidas",
+        name: "Shirt Full-Sleeve",
+        image: "./images/products/n3.jpg",
+        rating: 5,
+        price: 78
+    },
+    {
+        brand: "adidas",
+        name: "Shirt Half-Sleeve",
+        image: "./images/products/n4.jpg",
+        rating: 5,
+        price: 55
+    },
+    {
+        brand: "adidas",
+        name: "Shirt Full-Sleeve",
+        image: "./images/products/n5.jpg",
+        rating: 5,
+        price: 77
+    },
+    {
+        brand: "adidas",
+        name: "Half Pant",
+        image: "./images/products/n6.jpg",
+        rating: 5,
+        price: 45
+    },
+    {
+        brand: "adidas",
+        name: "Shirt Full-Sleeve",
+        image: "./images/products/n7.jpg",
+        rating: 5,
+        price: 77
+    },
+    {
+        brand: "adidas",
+        name: "Shirt Half-Sleeve",
+        image: "./images/products/n8.jpg",
+        rating: 5,
+        price: 75
+    }
+];
+
+
 //sproduct function
 function sproductFunction(event){
     window.location.href = "sproduct.html";
@@ -24,11 +141,29 @@ let shopImg = document.getElementsByClassName("shopImg");
 
 for(let i = 0; i < shopImg.length; i++){
     shopImg[i].addEventListener("click", function(){
-        sessionStorage.setItem("selectedImg", shopImg[i].src);
+        const selectedShopItem = shop[i];
+
+        sessionStorage.setItem("selectedImg", selectedShopItem.image);
+        sessionStorage.setItem("selectedBrand", selectedShopItem.brand);
+        sessionStorage.setItem("selectedName", selectedShopItem.name);
+        sessionStorage.setItem("selectedPrice", selectedShopItem.price);
     });
 }
 
 let selectedImg = sessionStorage.getItem("selectedImg");
+let selectedBrand = document.getElementById("shop-brand");
+let selectedName = document.getElementById("shop-name");
+let selectedPrice = document.getElementById("shop-price");
+
 if(selectedImg){
     MainImg.src = selectedImg;
+}
+if(selectedBrand){
+    selectedBrand.textContent = sessionStorage.getItem("selectedBrand");
+}
+if(selectedName){
+    selectedName.textContent = sessionStorage.getItem("selectedName");
+}
+if(selectedPrice){
+    selectedPrice.textContent = "$" + sessionStorage.getItem("selectedPrice");
 }
